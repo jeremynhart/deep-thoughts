a98a26915b87d235aa2586de95ca986ae664da20
Jeremy Hart <jeremynhart@gmail.com>
2023-05-04 08:59:25 -0500

    typeDefs update

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,7 +1,14 @@
-// import the gql tagged template function
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    friendCount: Int
+    thoughts: [Thought]
+    friends: [User]
+  }
 
   type Thought {
     _id: ID
@@ -20,9 +27,12 @@ const typeDefs = gql`
   }
 
   type Query {
+    users: [User]
+    user(username: String!): User
     thoughts(username: String): [Thought]
+    thought(_id: ID!): Thought
   }
 `;
 
-// export the typeDefs
 module.exports = typeDefs;
+
