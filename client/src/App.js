7a16885a1ea15bd3fc1f33beadce585176c5449f
Jeremy Hart <jeremynhart@gmail.com>
2023-05-05 21:44:04 -0500

    2.4.3: Set Up the Main URL Routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
+import Login from './pages/Login';
+import NoMatch from './pages/NoMatch';
+import SingleThought from './pages/SingleThought';
+import Profile from './pages/Profile';
+import Signup from './pages/Signup';
 
 const httpLink = createHttpLink({
   uri: '/graphql',
@@ -16,15 +22,42 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <div className="flex-column justify-flex-start min-100-vh">
-        <Header />
+      <Router>
+        <div className="flex-column justify-flex-start min-100-vh">
+          <Header />
         <div className="container">
-          <Home />
+          <Routes>
+            <Route
+              path="/"
+              element={<Home />}
+            />
+            <Route
+              path="/login"
+              element={<Login />}
+            />
+            <Route
+              path="/signup"
+              element={<Signup />}
+            />
+            <Route
+              path="/profile"
+              element={<Profile />}
+            />
+            <Route
+              path="/thought"
+              element={<SingleThought />}
+            />
+
+            <Route
+              path="*"
+              element={<NoMatch />}
+            />
+          </Routes>
         </div>
-        <Footer />
-      </div>
+          <Footer />
+        </div>
+      </Router>
     </ApolloProvider>
   );
 }
-
 export default App;
