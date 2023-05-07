4c3cb581fc8ac3504b2afaa1a325ceb652c96698
Jeremy Hart <jeremynhart@gmail.com>
2023-05-06 20:22:16 -0500

    2.5.6: Create and Implement User-Based Queries

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Auth from '../../utils/auth';
+
+const logout = event => {
+  event.preventDefault();
+  Auth.logout();
+};
 
 const Header = () => {
   return (
@@ -8,11 +14,21 @@ const Header = () => {
         <Link to="/">
           <h1>Deep Thoughts</h1>
         </Link>
-
         <nav className="text-center">
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Signup</Link>
-        </nav>
+        {Auth.loggedIn() ? (
+          <>
+            <Link to="/profile">Me</Link>
+            <a href="/" onClick={logout}>
+              Logout
+            </a>
+          </>
+        ) : (
+          <>
+            <Link to="/login">Login</Link>
+            <Link to="/signup">Signup</Link>
+          </>
+        )}
+      </nav>
       </div>
     </header>
   );
